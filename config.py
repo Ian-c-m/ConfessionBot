c05d63c442b35e09d71604d088b8a0754f7cbe76
Quackersian <54241767+quackersian@users.noreply.github.com>
2023-01-04 10:46:15 +0000

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+import logging
+script_version = "0.1.0"
+script_date = "2022-08-22"
+bot_name = "Some Bot Name Here"
+github_link = "Some GitHub Link Here"
+discord_server = "https://discord.gg/x7CyFRA5s6"
+logging_level = logging.DEBUG
